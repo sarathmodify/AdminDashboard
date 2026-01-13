ba6f6970452e3626852fc318f31792ee1331bd48
sarathmodify <sarathmanikam369@email.com>
2026-01-13 16:02:06 +0530

    fix: pwa serviceworker set issue

--- admindashboard/src/index.js
+++ admindashboard/src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import * as serviceWorkerRegistration from '../src/serviceWorkerRegistration';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
